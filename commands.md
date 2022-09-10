e0f2755af2b18181a09f19239787419e7ab7392c
ddivad195 <ddivad195@gmail.com>
2022-09-10 15:22:14 +0100

    feat: complete slash command migration

--- commands.md
+++ commands.md
@@ -4,31 +4,29 @@
 | Symbol      | Meaning                        |
 |-------------|--------------------------------|
 | [Argument]  | Argument is not required.      |
+| /Category   | This is a subcommand group.    |
 
-## Configuration
-| Commands              | Arguments | Description                                                |
-|-----------------------|-----------|------------------------------------------------------------|
-| toggleRemoveReactions |           | Toggle reactions being removed on in-progress suggestions. |
-| toggleShowVotes       |           | Toggle votes being displayed on in-progress suggestions.   |
-| toggleVotingDM        |           | Toggle DMs being sent upon voting for a suggestions.       |
-
-## Setup
-| Commands         | Arguments        | Description                                                  |
-|------------------|------------------|--------------------------------------------------------------|
-| configuration    |                  | Set the review or public channel to be used for suggestions. |
-| setChannel       | Channel, Channel | Set the review or public channel to be used for suggestions. |
-| setsuggstionrole | Role             | Set the minimum required role to make a suggestion.          |
-| setup            |                  | Configure a guild to use this bot.                           |
+## /Configuration
+| Commands              | Arguments                                                                  | Description                                                  |
+|-----------------------|----------------------------------------------------------------------------|--------------------------------------------------------------|
+| setChannel            | ChannelType, Channel                                                       | Set the review or public channel to be used for suggestions. |
+| setSuggstionRole      | Role                                                                       | Set the minimum required role to make a suggestion.          |
+| setup                 | SuggestionChannel, ReviewChannel, ShowVotes, ShowReactions, ConfirmationDM | Configure a guild to use this bot.                           |
+| toggleRemoveReactions |                                                                            | Toggle reactions being removed on in-progress suggestions.   |
+| toggleShowVotes       |                                                                            | Toggle votes being displayed on in-progress suggestions.     |
+| toggleVotingDM        |                                                                            | Toggle DMs being sent upon voting for a suggestions.         |
+| view                  |                                                                            | View guild configuration                                     |
 
 ## Suggestions
 | Commands  | Arguments  | Description                                                      |
 |-----------|------------|------------------------------------------------------------------|
 | setStatus | ID, Status | Set the status for a suggestion (backup for interaction buttons) |
-| stats     |            | Set the status for a suggestion (backup for interaction buttons) |
+| stats     |            | Get stats about guild suggestions                                |
 | suggest   | Suggestion | Make a suggestion.                                               |
 
 ## Utility
-| Commands | Arguments | Description          |
-|----------|-----------|----------------------|
-| Help     | [Command] | Display a help menu. |
+| Commands | Arguments | Description                 |
+|----------|-----------|-----------------------------|
+| Help     | [Command] | Display a help menu.        |
+| info     |           | Bot info for Suggestion-Bot |
 
--- src/main/kotlin/me/ddivad/suggestions/commands/GuildConfigCommands.kt
+++ src/main/kotlin/me/ddivad/suggestions/commands/GuildConfigCommands.kt
@@ -1,49 +1,63 @@
 package me.ddivad.suggestions.commands
 
 import dev.kord.core.entity.channel.TextChannel
-import me.ddivad.suggestions.conversations.ConfigurationConversation
 import me.ddivad.suggestions.dataclasses.BotPermissions
 import me.ddivad.suggestions.dataclasses.Configuration
 import me.ddivad.suggestions.embeds.createConfigurationEmbed
+import me.jakejmattson.discordkt.arguments.BooleanArg
 import me.jakejmattson.discordkt.arguments.ChannelArg
 import me.jakejmattson.discordkt.arguments.ChoiceArg
 import me.jakejmattson.discordkt.arguments.RoleArg
-import me.jakejmattson.discordkt.commands.commands
+import me.jakejmattson.discordkt.commands.subcommand
+import me.jakejmattson.discordkt.dsl.edit
 
 @Suppress("unused")
-fun guildConfigCommands(configuration: Configuration) = commands("Setup", BotPermissions.Admin) {
-    text("setup") {
-        description = "Configure a guild to use this bot."
-        execute {
+fun guildConfigCommands(configuration: Configuration) = subcommand("Configuration", BotPermissions.Admin) {
+    sub("setup", "Configure a guild to use this bot.") {
+        execute(
+            ChannelArg("SuggestionChannel", "Channel suggestions will be published to"),
+            ChannelArg("ReviewChannel", "Suggestion review channel"),
+            BooleanArg("ShowVotes", "yes", "no", "Show votes on public suggestions"),
+            BooleanArg("ShowReactions", "yes", "no", "Show reactions on public suggestions during voting"),
+            BooleanArg("ConfirmationDM", "yes", "no", "Send confirmation dm when voting")
+        ) {
             if (configuration.hasGuildConfig(guild.id)) {
                 respond("Guild configuration exists. To modify it use the commands to set values.")
                 return@execute
             }
-            ConfigurationConversation(configuration)
-                .createConfigurationConversation(guild)
-                .startPublicly(discord, author, channel)
-            respond("${guild.name} setup with the following configuration:")
-            respond { configuration[guild.id]?.let { createConfigurationEmbed(guild, it) } }
+            val (suggestionChannel, suggestionReviewChannel, showVotes, showReactions, confirmationDM) = args
+            val guildConfiguration = configuration.setup(
+                guild,
+                suggestionChannel as TextChannel,
+                suggestionReviewChannel as TextChannel,
+                showVotes,
+                showReactions,
+                confirmationDM
+            )
+            respondPublic("${guild.name} setup with the following configuration:") {
+                if (guildConfiguration != null) {
+                    createConfigurationEmbed(
+                        guild, guildConfiguration
+                    )
+                }
+            }
         }
     }
 
-    text("setsuggstionrole") {
-        description = "Set the minimum required role to make a suggestion."
+    sub("setSuggstionRole", "Set the minimum required role to make a suggestion.") {
         execute(RoleArg) {
             if (!configuration.hasGuildConfig(guild.id)) {
                 respond("Please run the **configure** command to set this initially.")
                 return@execute
             }
             val role = args.first
-            configuration[guild.id]?.requiredSuggestionRole = role.id
-            configuration.save()
+            configuration.edit { guildConfigurations[guild.id]?.requiredSuggestionRole = role.id }
             respond("Role set to: **${role.name}**")
         }
     }
 
-    text("setChannel") {
-        description = "Set the review or public channel to be used for suggestions."
-        execute(ChoiceArg("Channel", "public", "review"), ChannelArg<TextChannel>("Channel")) {
+    sub("setChannel", "Set the review or public channel to be used for suggestions.") {
+        execute(ChoiceArg("ChannelType", "public", "review"), ChannelArg<TextChannel>("Channel")) {
             if (!configuration.hasGuildConfig(guild.id)) {
                 respond("Please run the **configure** command to set this initially.")
                 return@execute
@@ -52,21 +66,54 @@ fun guildConfigCommands(configuration: Configuration) = commands("Setup", BotPer
             val config = configuration[guild.id]
             when (option.lowercase()) {
                 "public" -> {
-                    config?.suggestionChannel = channel.id
+                    configuration.edit { guildConfigurations[guild.id]?.suggestionChannel = channel.id }
                 }
 
                 "review" -> {
-                    config?.suggestionReviewChannel = channel.id
+                    configuration.edit { guildConfigurations[guild.id]?.suggestionReviewChannel = channel.id }
                 }
             }
-            configuration.save()
             respond("Set the **$option** channel to ${channel.mention}")
         }
     }
 
-    text("configuration") {
-        description = "Set the review or public channel to be used for suggestions."
-        requiredPermissions = BotPermissions.Staff
+    sub("toggleShowVotes", "Toggle votes being displayed on in-progress suggestions.") {
+        execute {
+            if (!configuration.hasGuildConfig(guild.id)) {
+                respond("Please run the **configure** command to set this initially.")
+                return@execute
+            }
+            val guildConfig = configuration[guild.id] ?: return@execute
+            guildConfig.showVotes = !guildConfig.showVotes
+            respond("Toggled displaying votes ${if (guildConfig.showVotes) "**On**" else "**Off**"}")
+        }
+    }
+
+    sub("toggleRemoveReactions", "Toggle reactions being removed on in-progress suggestions.") {
+        execute {
+            if (!configuration.hasGuildConfig(guild.id)) {
+                respond("Please run the **configure** command to set this initially.")
+                return@execute
+            }
+            val guildConfig = configuration[guild.id] ?: return@execute
+            guildConfig.removeVoteReactions = !guildConfig.removeVoteReactions
+            respond("Toggled removing reactions ${if (guildConfig.removeVoteReactions) "**On**" else "**Off**"}")
+        }
+    }
+
+    sub("toggleVotingDM", "Toggle DMs being sent upon voting for a suggestions.") {
+        execute {
+            if (!configuration.hasGuildConfig(guild.id)) {
+                respond("Please run the **configure** command to set this initially.")
+                return@execute
+            }
+            val guildConfig = configuration[guild.id] ?: return@execute
+            guildConfig.sendVotingDM = !guildConfig.sendVotingDM
+            respond("Toggled sending DMs ${if (guildConfig.sendVotingDM) "**On**" else "**Off**"}")
+        }
+    }
+
+    sub("view", "View guild configuration") {
         execute {
             val config = configuration[guild.id] ?: return@execute
             respond { createConfigurationEmbed(guild, config) }
